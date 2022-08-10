aa055562c03477129b7537b57c209802244189df
sahil-555 <sahilkhandual555@gmail.com>
2022-08-10 13:24:02 +0530

    commit

---
+++ day2/src/lti/array/array.java
@@ -0,0 +1,16 @@
+package lti.array;
+
+import java.util.Arrays;
+
+public class array {
+
+	public static void main(String[] args) {
+		String s1[]= {"pune","delhi","chennai"};
+		Arrays.sort(s1);
+		for (int i=0;i<s1.length;i++) {
+			System.out.println(s1[i].toUpperCase());
+		}
+
+	}
+
+}
